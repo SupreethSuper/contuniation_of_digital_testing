43dd02de73731ff8dcebc57d5429666f9828fe44
SuperSupreeth <supreeth.sandri@gmail.com>
2026-02-05 17:57:53 -0700

    orginal pli_test added for accidental corruption purpose

---
+++ pli_test_original.c
@@ -0,0 +1,16 @@
+#include "acc_user.h"
+
+int pli_function(char * value);   // declare prototype
+
+pli_test_next()
+{
+  int next_value;
+  char *data_out_str;
+
+  // get the current value of data_out
+  data_out_str = "5";
+
+  next_value = pli_function(data_out_str);
+
+  // set the value of data_in
+}
