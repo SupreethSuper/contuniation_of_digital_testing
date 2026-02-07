83d6f325bc11128dfc1a24d72e4dad04feff0f69
SuperSupreeth <supreeth.sandri@gmail.com>
2026-02-06 19:59:19 -0700

    all bugs fixed!!

--- pli_test.c
+++ pli_test.c
@@ -10,14 +10,20 @@
 
 // 3) Debug printf statements are added
 
+// ===================================MAJOR CHANGES FOR TEST 4===================================
+// 1) Change the format specifier in acc_fetch_value from "%h" to "%d" as a trial. Found out that %d is fetched in pli function
+// 2) Change  static s_setval_value value_s = {accInt}; to  static s_setval_value value_s = {accIntVal};, bug fixed
+// 3) my_setup() is commented out as it is not used in this test, as it was doing nothing, and no changes were observed
+// ===============================END OF MAJOR CHANGES FOR TEST 4===================================
+
 int pli_function(char * value);   // declare prototype
 
 int counter = 0; // global counter variable
 
-my_setup()
-{
-  counter = 0; // initialize counter to 0
-}
+// my_setup()
+// {
+//   counter = 0; // initialize counter to 0
+// }
 
 pli_test_next()
 {
@@ -30,20 +36,23 @@ pli_test_next()
 
   
 
-  char *data_out_str = acc_fetch_value(h_data_out, "%h", buffer_for_acc_fetch_value); // fetch the current value of data_out and store it in data_out_str
+  // char *data_out_str = acc_fetch_value(h_data_out, "%h", buffer_for_acc_fetch_value); // fetch the current value of data_out and store it in data_out_str
 
-  // get the current value of data_out
-  // data_out_str = "5"; //old functionality scrapped
-  printf("Current value of data_out: %h\n", data_out_str); // print the current value of data_out
-  printf("The buffer contains: %h\n", buffer_for_acc_fetch_value); // print the contents of the buffer
+  char *data_out_str = acc_fetch_value(h_data_out, "%d", buffer_for_acc_fetch_value); // changed the format specifier to "%d" as a trial. Found out that %d is fetched in pli function
+
+  printf("Current value of data_out: %s\n", data_out_str);
+  printf("The buffer contains: %s\n", buffer_for_acc_fetch_value);
 
   next_value = pli_function(data_out_str);
 
-  static s_setval_delay delay_s = {{0,1,0,0.0}, accNoDelay}; // set the delay for the value assignment
-  static s_setval_value value_s = {accIntVal}; // set the type and initial value for the value assignment. Was accInt before but changed to accIntVal for error dealing
-  value_s.value.integer = (next_value & 0xFFFF); // set the integer value to be assigned to data_in
-  acc_set_value(h_data_in, &value_s, &delay_s); //error_dealth with
-  counter++;
+  static s_setval_delay delay_s = {{0,0,0,0.0}, accNoDelay};   // (also fixed the "1" -> "0")
+  static s_setval_value value_s = {accIntVal};
+
+  value_s.value.integer = (next_value & 0xFFFF);
+  acc_set_value(h_data_in, &value_s, &delay_s);
+
+
+  //counter++;
  
 
   // set the value of data_in
