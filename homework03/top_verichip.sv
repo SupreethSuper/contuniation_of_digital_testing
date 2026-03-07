3530097ea16bbd98077a06d8a5df5f82d267c7b0
SuperSupreeth <supreeth.sandri@gmail.com>
2026-03-06 19:45:36 -0700

    clk

--- homework03/top_verichip.sv
+++ homework03/top_verichip.sv
@@ -116,38 +116,38 @@ initial begin
    `CLEAR_ALL
    `CHIP_RESET
 
-@negedge(clk)
+@(negedge clk)
    maroon <= 1'b0;
-@negedge(clk)
+@(negedge clk)
    gold <= 1'b1;
 
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'hFF_FF, 2'b11, 1'b1)
    `SET_READ(VCHIP_ALU_LEFT_ADDR, 1'b1)
    `CHECK_VAL(16'hFF_FF)
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'hFF_FF, 2'b11,1'b1)
    `CLEAR_BUS
 
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_RIGHT_ADDR, 16'hFF_FF, 2'b11,1'b1)
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'h30_CD, 2'b10,1'b1)
    `SET_READ(VCHIP_ALU_LEFT_ADDR, 1'b1);
    `CHECK_VAL(16'h30_FF)
    `CLEAR_BUS
 
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'hFF_FF, 2'b11,1'b1)
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'hCD_30, 2'b01,1'b1)
    `SET_READ(VCHIP_ALU_LEFT_ADDR, 1'b1);
    `CHECK_VAL(16'hFF_30)
    `CLEAR_BUS
 
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'hFF_FF, 2'b11,1'b1)
-@negedge(clk)
+@(negedge clk)
    `SET_WRITE(VCHIP_ALU_LEFT_ADDR, 16'hCD_30, 2'b00,1'b1)
    `SET_READ(VCHIP_ALU_LEFT_ADDR, 1'b1);
    `CHECK_VAL(16'hFF_FF)
