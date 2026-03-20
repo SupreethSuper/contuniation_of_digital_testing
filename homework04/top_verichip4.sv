a6080ef4e68f92146fe86325216aecc4f4c3bd63
SuperSupreeth <supreeth.sandri@gmail.com>
2026-03-19 20:02:05 -0700

    slight logical error attempt to fix (ATF), version reg acess alias test pass 2

--- homework04/top_verichip4.sv
+++ homework04/top_verichip4.sv
@@ -745,24 +745,25 @@ begin
    #10;//Attempt to read 0000 from ALU_Left
    `SET_READ(VCHIP_VER_ADDR, 1'b0)
    #10;// Make sure 0000 is read back from ALU left
-   `CHECK_VAL({4'b0000, VCHIP_ALU_VER, VCHIP_MAJ_VER, VCHIP_MIN_VER})
+   `CHECK_VAL(16'h00_00)
    // Similarly, apply this method for other test inputs within the same combination.
    `SET_WRITE(VCHIP_VER_ADDR, 16'hAAAA, 2'b11, 1'b1)
    #10;
    `SET_READ(VCHIP_VER_ADDR, 1'b0)
    #10;
-   `CHECK_VAL({4'b0000, VCHIP_ALU_VER, VCHIP_MAJ_VER, VCHIP_MIN_VER})
+   `CHECK_VAL(16'h00_00)
 
    `SET_WRITE(VCHIP_VER_ADDR, 16'h5555, 2'b11, 1'b1)
    #10;
    `SET_READ(VCHIP_VER_ADDR, 1'b0)
    #10;
-   `CHECK_VAL({4'b0000, VCHIP_ALU_VER, VCHIP_MAJ_VER, VCHIP_MIN_VER})
+   `CHECK_VAL(16'h00_00)
+
    `SET_WRITE(VCHIP_VER_ADDR, 16'hFFFF, 2'b11, 1'b1)
    #10;
    `SET_READ(VCHIP_VER_ADDR, 1'b0)
    #10;
-   `CHECK_VAL({4'b0000, VCHIP_ALU_VER, VCHIP_MAJ_VER, VCHIP_MIN_VER})
+   `CHECK_VAL(16'h00_00)
 
   $display("\n\n\nxxxxxxxxxxxxxxxxxxxxxx end of access with chip select = 1 xxxxxxxxxxxxxxxxxxxxxx"); 
 
