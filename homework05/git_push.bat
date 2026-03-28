785eb02d37fcb6fada59679cee0c7d1f3b71702c
SuperSupreeth <supreeth.sandri@gmail.com>
2026-03-27 23:14:43 -0700

    h

---
+++ homework05/git_push.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd /d "C:\Users\supre\OneDrive - Arizona State University\Digital Verification & Testing\hw4\contuniation_of_digital_testing\homework05"
+
+git add .
+git commit -m "h"
+git push --force
